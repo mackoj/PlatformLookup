c977baf21e75773c5d5b25f0514f68912fd5cd42
Jeffrey Macko <github.jm@macko.fr>
2019-12-14 10:16:18 +0100

    Add Sheel

---
+++ Sources/Shell/Shell.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jeffrey Macko on 14/12/2019.
+//
+
+import Foundation
+
+public func shell(_ command: String) -> String {
+    let task = Process()
+    task.launchPath = "/bin/bash"
+    task.arguments = ["-c", command]
+
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+
+    return output
+}
