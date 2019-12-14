3845abae8f3f7d6bb6ac1c4648e59d471c21e437
Jeffrey Macko <github.jm@macko.fr>
2019-12-14 23:31:07 +0100

    Add a test for shell

--- Tests/ShellTests/ShellTests.swift
+++ Tests/ShellTests/ShellTests.swift
@@ -1,20 +1,16 @@
-import XCTest
 import SnapshotTesting
+import XCTest
+
 @testable import Shell
 
 final class ShellTests: XCTestCase {
-  
   func test_SimpleTest() {
     do {
       try shell("touch testFile.txt")
       let lsOutput = try shell("ls testFile.txt")
       assertSnapshot(matching: lsOutput, as: .description)
-    } catch {
-      XCTAssert(false, error.localizedDescription)
-    }
+    } catch { XCTAssert(false, error.localizedDescription) }
   }
 
-  static var allTests = [
-    ("test_SimpleTest", test_SimpleTest)
-  ]
+  static var allTests = [("test_SimpleTest", test_SimpleTest),]
 }
