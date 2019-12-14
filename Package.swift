0da0b0e3277450dc9070d560a69bf1d4bfb21fc2
Jeffrey Macko <github.jm@macko.fr>
2019-12-14 11:05:12 +0100

    Paving the way for more tests

--- Package.swift
+++ Package.swift
@@ -19,14 +19,15 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/mrackwitz/Version.git", from: "0.7.2"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.0")
   ],
   targets: [
     .target(name: "PlatformLookup", dependencies: ["SimulatorControl", "Shell"]),
     .target(name: "Shell"),
     .target(name: "SimulatorControl", dependencies: ["Version"]),
-    .testTarget(name: "PlatformLookupTests", dependencies: ["PlatformLookup"]),
+    .testTarget(name: "PlatformLookupTests", dependencies: ["PlatformLookup", "SnapshotTesting"]),
     .testTarget(name: "SimulatorControlTests", dependencies: ["SimulatorControl"]),
-    .testTarget(name: "ShellTests", dependencies: ["Shell"]),
+    .testTarget(name: "ShellTests", dependencies: ["Shell", "SnapshotTesting"]),
   ],
   swiftLanguageVersions: [.v5]
 )
