69959c2518b7a9d4ffdfec99ed6e0f93750c9f6c
Jeffrey Macko <github.jm@macko.fr>
2019-12-17 12:49:04 +0100

    Fix bitrise lookup

---
+++ bitrisePlatformLookup.swift
@@ -0,0 +1,29 @@
+import Foundation
+import PlatformLookup  // mackoj/SimulatorControl
+import Shell
+
+func findDevicePlatform(_ args: [String]) throws {
+  let platform = try PlatformLookup.findADeviceForLastOSVersion(.iPhone)
+  _ = try shell(
+    "envman add --key PLATFORM_LOOKUP_DEVICE_MODEL --value \"\(platform.devices.last!.name)\""
+  )
+  _ = try shell(
+    "envman add --key PLATFORM_LOOKUP_OS_VERSION --value \"\(platform.runtime.version)\""
+  )
+  let platformString = try PlatformLookup.format(
+    platform,
+    deviceFamily: .iPhone
+  )
+  _ = try shell(
+    "envman add --key PLATFORM_LOOKUP_PLATFORM --value \"\(platformString)\""
+  )
+  print(platformString)
+  exit(EXIT_SUCCESS)
+}
+func test() {
+  do { try findDevicePlatform(CommandLine.arguments) }
+  catch {
+    print(error.localizedDescription)
+    exit(EXIT_FAILURE)
+  }
+}
