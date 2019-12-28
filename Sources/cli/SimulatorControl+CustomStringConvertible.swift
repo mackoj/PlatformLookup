563a7c651606bda21fcdcced0231f0aaf703878d
Jeffrey Macko <github.jm@macko.fr>
2019-12-29 00:21:33 +0100

    Improve AutoDescription.swift

--- Sources/cli/SimulatorControl+CustomStringConvertible.swift
+++ Sources/cli/SimulatorControl+CustomStringConvertible.swift
@@ -2,21 +2,9 @@ import Foundation
 import SimulatorControl
 import PlatformLookup
 
-extension Device: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
-extension Devicetype: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
-extension Pair: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
-extension Runtime: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
-extension SimulatorControl: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
-extension Platform: CustomStringConvertible {
-  public var description: String { return DebugAny.snap(self) }
-}
+extension Device: CustomStringConvertible {}
+extension Devicetype: CustomStringConvertible {}
+extension Pair: CustomStringConvertible {}
+extension Runtime: CustomStringConvertible {}
+extension SimulatorControl: CustomStringConvertible {}
+extension Platform: CustomStringConvertible {}
